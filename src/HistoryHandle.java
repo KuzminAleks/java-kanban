c4725ed993ebbb4fc79db7fa2995bde23027186e
ofermad <redturner@mail.ru>
2024-06-28 01:10:28 +0300

    Исправить модификатор

--- src/HistoryHandle.java
+++ src/HistoryHandle.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class HistoryHandle extends FormatAdapters implements HttpHandler {
-    final public TaskManager manager;
+    final private TaskManager manager;
 
     final Gson gson = new GsonBuilder()
             .setPrettyPrinting()
